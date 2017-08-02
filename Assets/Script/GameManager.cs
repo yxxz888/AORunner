3e697edfd26348101e837c54f19f07b4ffe08325
taojiayang <taojiayang@aobi.com>
2017-08-02 18:03:12 +0800

    add script

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Player player;
+    public Camera followCamera;
+	// Use this for initialization
+	void Start () {
+        followCamera.transform.parent = player.transform;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey("Jump"))
+        {
+            
+        }
+        player.forward();
+	}
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Animator animator;
+
+    private bool isStart;
+ 
+	// Use this for initialization
+	void Start () {
+        animator = this.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void startGame()
+    {
+        isStart = true;
+        animator.SetBool("Start", true);
+    }
+
+    public void forward()
+    {
+        if(isStart)
+            this.transform.Translate(new Vector3(0,0,0.1f));
+    }
+}
